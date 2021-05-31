d2d657452d8705a609e970a74c009663389b950e
xiphiasnonus <foster.pianist@gmail.com>
2021-05-31 11:16:48 -0400

    add lesson notes, minor tweaks

--- play.c
+++ play.c
@@ -2,7 +2,7 @@
  * Created by Geegbear  
  * 29/05/2021  
  * "Frequency/millisecond calculator"
- * To be implemented along the function 'sound(...)' from the LibDos turbo C Library    
+ * To be implemented along the function 'sound(...)' from the LibTC
  * Such implementation has not yet been tested!
  * 
  */   
@@ -39,29 +39,29 @@ enum
     H,
 };
 
+// TODO: call it 'bpm' or 'tempo'
+const float metronome = 60.0f;// variable in bpm(beats per second)
 
-const float metronome = 60.0f;// variable in bps(beats per second)
 
-
-float freq(float note)
+float freq(int note)
 {
     //for pitch...:
     const float half_step = 1.05946f; //in equal temperament
-    float freq_ratio = pow(half_step, note);    
+    float freq_ratio = powf(half_step, (float)note);
     float middle_c = 261.626f;
     float frequency = middle_c * freq_ratio;
-    return frequency; 
+    return frequency;
 }
 
-
-float beat(float length) //
+// naming: length -> note_value, beat -> notevalue_to_ms
+float beat(float length)
 {
     //for rhythm...:
     float std_bps = 60.00f; //bps (standarized for the purpose of this calculation)
 
     // 4.00 = constant(k)
-    float ms = (length * 1000.00f) / 4.00f; 
-
+    float ms = (length * 1000.00f) / 4.00f;
+    
     //calculating milliseconds... length ∝ 1/ms -> 4, which is our constant
    
     return std_bps * ms / metronome;  //TODO: do inverse proportion of this value against variable declaration 'float length', 
