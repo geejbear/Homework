f4e02676466b51d9af4c4b23d80621df64fc09df
xiphiasnonus <foster.pianist@gmail.com>
2021-06-07 12:37:58 -0400

    lesson notes 2

--- hunt.c
+++ hunt.c
@@ -6,42 +6,73 @@
  * Description : game
  *
  * TODO LIST
- * - X keep the player from going off screen
- * - _ try somthing simple like place an item(s) the player collects
- * - _ 
+ * X keep the player from going off screen
+ * - try somthing simple like place an item(s) the player collects
+ * - use play() from play module
+ * - write ascii.h with enum for 1-31 ascii glyph
+ * - write a function clamp
+ * - simplify: one gem -> collect it
+ * - - random location
+ *
  */
 #include <dos.h>
 #include <conio.h>
-#include <stdbool.h> 
+#include <stdbool.h>
+#include <stdlib.h>
 
+// TODO: better CHAR_JEWEL
 #define JEWEL 4
 #define SMILY 2
+
 // TYPES, STRUCTS, ENUMS
+
 enum state
 {
     STATE_EMERALD,
     STATE_RUBY,
     STATE_DIAMOND,
-}; 
-// DATA
+};
+
+
+// DATA (state)
 
 // global variables are initialized to 0 by default
 int player_x = 10;
 int player_y = 10;
 
+
 // FUNCTIONS
 
+
+// 15
+// clamp(15, 5, 12) -> 12
+
+// ??? clamp(value, min, max)
+//{
+    
+//}
+
+
+// TODO: use a verb
 int river(int n)
 {
-    int fib[n];
+    int *fib = malloc( n * sizeof(int) );
+    
+    // TODO: validate n
+    
     fib[0] = 1;
     fib[1] = 1;
+    
     for (int i = 2; i < n; i++) {
         fib[i] = fib[i - 1] + fib[i - 2];
         cprintf("%d\n", fib[i]);
     }
+    
+    free(fib);
+    
     return 0;
 }
+
 void collect(int x, int y, int COLOR)
 {
     gotoxy(x, y);
@@ -57,6 +88,7 @@ void sound_collect()
     sound(329, 115);
     //sound(440, 115);
 }
+
 void jewel(int x, int y, int COLOR)
 {
     gotoxy(x, y);
@@ -65,14 +97,13 @@ void jewel(int x, int y, int COLOR)
     textcolor(LIGHTGRAY);
 }
 
-
 void key_hit() // this function: every time a hit is hit
 {
     clrscr();
     int key = getch();
 
     switch (key) {
-        case 'w':
+        case 'w': // TODO: optimize logic for wasd
             --player_y;
             if (player_y < 1) {
                 ++player_y;
@@ -99,7 +130,9 @@ void key_hit() // this function: every time a hit is hit
         default:
             break;
     }
-
+    
+    // TODO: the player has moved, does something happen?
+    
     //cprintf("%d %d", player_x, player_y);
     gotoxy(player_x, player_y);
     putch(SMILY);
@@ -108,13 +141,14 @@ void key_hit() // this function: every time a hit is hit
 int main()
 {
     textmode(C40);
-    setscreensize(20, 20);
+    setscreensize(20, 20); // TODO: use #define
     setcursor(0);
     initdos();
     
     gotoxy(player_x, player_y);
     putch(SMILY);
 
+    // TODO: num_emeralds, etc. (number of emeralds)
     int emerald_count = 0;
     int ruby_count = 0;
     int diamond_count = 0;
@@ -135,16 +169,15 @@ int main()
         textcolor(LIGHTBLUE);
         river(23);
 
-      
+        // TODO: refactor (put into its own function)
         gotoxy(5, 1);
         textcolor(GREEN);
-        cprintf("%c=%d ", JEWEL, emerald_count);
+        cprintf("%c=%d ", JEWEL, emerald_count); // TODO: DRY
         textcolor(RED);
         cprintf("%c=%d ", JEWEL, ruby_count);
         textcolor(LIGHTCYAN);
         cprintf("%c=%d", JEWEL, diamond_count);
-        textcolor(LIGHTGRAY);
-
+        textcolor(LIGHTGRAY); // TODO: remove
         
         int state = STATE_EMERALD;
         
@@ -184,7 +217,6 @@ int main()
         // add code to check for a key press (kbhit & getch)
         if (kbhit()) {
             key_hit();
-            
         }
         
         refresh();
