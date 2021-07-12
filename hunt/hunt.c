09d3b0dc84b733332c59a369979f0df31e15a1be
xiphiasnonus <foster.pianist@gmail.com>
2021-07-12 11:27:33 -0400

    lesson notes and changes

--- hunt/hunt.c
+++ hunt/hunt.c
@@ -44,11 +44,17 @@
 #define MIN_X           1
 #define MIN_Y           1
 
-#define MAX_NUM_ITEMS   4
-
 // =============================================================================
 // STRUCTS / TYPES / ENUMS
 
+enum
+{
+    ENT_PLAYER,
+    ENT_FLAT,
+    ENT_SHARP,
+    ENT_NATURAL,
+    ENT_NUM_ENTITIES
+};
 
 typedef struct
 {
@@ -56,7 +62,7 @@ typedef struct
     int y;
     int ch;
     int color;
-    bool active;
+    bool active; // gems: active = on the board
 } entity_t;
 
 // TODO: function that makes a new entity_t
@@ -65,11 +71,12 @@ typedef struct
 // =============================================================================
 // DATA (state) global variables are initialized to 0 by default
 
-entity_t entities[MAX_NUM_ITEMS]; 
-int colors[MAX_NUM_ITEMS] = {LIGHTGRAY, MAGENTA, RED, CYAN};
-int items[MAX_NUM_ITEMS] = {ASCII_FACE1, 'b', '#', ASCII_SECTION}; 
+entity_t entities[ENT_NUM_ENTITIES];
 
-bool item_collected = false; // this gets replaced with struct member (active)
+// the values that are used to initialize each entities .ch and .color members
+// (the default values)
+const int colors[ENT_NUM_ENTITIES] = {LIGHTGRAY, MAGENTA, RED, CYAN};
+const int items[ENT_NUM_ENTITIES] = {ASCII_FACE1, 'b', '#', ASCII_SECTION};
 
 
 
@@ -83,44 +90,35 @@ void print_objective()
     cprintf("G%c", ASCII_UPARROW);
 }
 
+// TODO: try var note, and note + 1 or genericness, or note param
 void sound_collect()
 {
     const int tempo = 220; 
     const int note_value = 16; 
-    const int octave = 4; 
+    const int octave = 4;
     
     for (int i = 0; i < 2; i++) {
         play(NOTE_G, octave, note_value, tempo);
         play(NOTE_G_SHARP, octave, note_value, tempo);
     }
-
 }
 
-void draw_level()
+// TODO: draw entity function
+
+void draw_entities()
 {
-    if (!item_collected) { //could this be dryer like an array?
-        for (int i = 0; i < MAX_NUM_ITEMS; i++) {
-            gotoxy(entities[i].x, entities[i].y);
-            textcolor(colors[i]);
-            putch(items[i]);
-        }    
-    }    
-
+    for (int i = 0; i < ENT_NUM_ENTITIES; i++) {
+        
+        if ( !entities[i].active ) {
+            continue;
+        }
+        
+        gotoxy(entities[i].x, entities[i].y);
+        textcolor(colors[i]);
+        putch(items[i]);
+    }
 }
 
-
-void collect_gem()
-{
-    if ( !item_collected && entities[2].x == entities[0].x && entities[2].y == entities[0].y ) {  // player is on gem!
-        // do collect gem stuff
-        item_collected = true;
-        sound_collect();
-        gotoxy(entities[0].x, entities[0].y);
-        textcolor(LIGHTGRAY);
-        putch(ASCII_FACE1);
-    } 
-    
-}
 
 // returns a random coordinate between 1 and max_value
 int random_coord(int max_value)
@@ -128,15 +126,14 @@ int random_coord(int max_value)
     return (rand() % max_value) + 1;
 }
 
-void draw_random_entities()
+void ranomize_entity_coords()
 {
     // this is the new version
-    for (int i = 0; i <= 3; i++)
+    for (int i = 0; i < ENT_NUM_ENTITIES; i++)
     {
         entities[i].x = random_coord(SCREEN_W);
         entities[i].y = random_coord(SCREEN_H);
     }
-    
 }
 
 void keep_in_bounds(int *x, int *y)
@@ -145,26 +142,49 @@ void keep_in_bounds(int *x, int *y)
     clamp(y, MIN_Y, SCREEN_H);
 }
 
-void redraw_level()
+bool are_entities_overlapping(entity_t *e1, entity_t *e2)
 {
+    return e1->x == e2->x && e1->y == e2->y;
+}
+
+void draw_everything()
+{
+    clrscr();
+    print_objective();
+    draw_entities();
+}
+
+// TODO: Objective: Collect a ? (sharp, flat, natural)
+// maybe: Sharps: 3 Flat: 5 Naturals: 10
+
+void update_game()
+{
+    // (for readability)
+    entity_t * player = &entities[ENT_PLAYER];
+    entity_t * sharp = &entities[ENT_SHARP];
+    
+    if ( are_entities_overlapping(player, sharp) ) {
+        
+        sharp->active = false; // collected it
    
-    if (item_collected && entities[2].x != entities[0].x && entities[2].y != entities[0].y) {
-        item_collected = false;
-        clrscr();
-        draw_random_entities();
-        draw_level();
+        draw_everything(); // hack so that you see the player on the gem spot
+        refresh();
+        sound_collect();
+        sleep(1);
+        
+        ranomize_entity_coords();
+        sharp->active = true; // uncollect it
     } 
 }
 
 void key_hit(int key) 
 {
-    switch (key) {
+    switch (key) { // TODO: use enum values (ENT_PLAYER instead of 0)
         case 'w': --entities[0].y; break;
         case 's': ++entities[0].y; break;
         case 'a': --entities[0].x; break;
         case 'd': ++entities[0].x; break;
         default: break;
-
     }
     
     keep_in_bounds(&entities[0].x, &entities[0].y);  
@@ -172,6 +192,7 @@ void key_hit(int key)
 
 int main()
 {
+    setscale(4);
     textmode(C40);
     setcursor(0);
     setscreensize(SCREEN_W, SCREEN_H);
@@ -180,23 +201,28 @@ int main()
     srand((unsigned)time(NULL));
     
     // TODO: set entities' appearance
+
+    // assign each entity its default color
+    for ( int i = 0; i < ENT_NUM_ENTITIES; i++ ) {
+        entities[i].ch = items[i];
+        entities[i].color = colors[i];
+        entities[i].active = true;
+    }
+
+    ranomize_entity_coords();
     
-    draw_random_entities();
-    draw_level();
-    
-    // game loop structure:
+    draw_everything();
+        
+    // game loop structure, every frame:
     // 1. get and handle inputs (keyboard, mouse)
     // 2. update the game (logic stuff)
     // 3. redraw
     while (1) {
         
         if (kbhit()) {
-            clrscr();
             key_hit(getch());
-            print_objective();
-            draw_level();
-            collect_gem();
-            redraw_level();
+            update_game();
+            draw_everything();
         }
         
         refresh();
