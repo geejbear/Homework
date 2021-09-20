c99aa49471d58e9ff80b88b2894f0f83298459dc
xiphiasnonus <foster.pianist@gmail.com>
2021-09-20 10:44:52 -0400

    lesson

--- DataStructs/xpense/xpense.c
+++ DataStructs/xpense/xpense.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_STACK_SIZE 10
+#define MAX_STACK_SIZE 10 // TODO: rename (it's not a stack) MAX_EXPENSES
+// TODO: something large like 1000
 
 #if 0
 float add(float nums[MAX_STACK_SIZE], float n)
@@ -22,14 +23,17 @@ int main()
 {    
     //initializing a new array
  
-    float array[MAX_STACK_SIZE];
+    float array[MAX_STACK_SIZE]; // more descriptive name: expenses
     float amount = 0;
-    int index = 0;
+    int index = 0; // rename: num_expenses
 
+    // TODO: only enter as many expenses as user has/wants
     // 1. enter amount spent
+    printf("enter \"0\" to stop\n"); // for example
 	while (index < MAX_STACK_SIZE) {
         
-        printf("enter the amount spent:\n");
+        printf("enter the amount spent:\n"); // print once at the start ?
+        // for each line: "amount: %f" ?
 	    scanf("%f", &amount); 
         printf("\n");
         array[index] = amount;
@@ -37,6 +41,7 @@ int main()
     }
     
     //values are temporarily listed on the prompter
+    // TODO: use num_expenses to only print those entered
     for (int i = 0; i < MAX_STACK_SIZE; i++) { 
         printf("€%.2f\n", array[i]);
     }
@@ -44,13 +49,13 @@ int main()
     // 2. save values in file
 	
     #if 1
-    FILE * fp;
-	fp = fopen("xpense.txt", "w");
+    FILE * fp = fopen("xpense.txt", "w");
 	if (fp == NULL) {
-		printf("fopen failed");
+		printf("fopen failed\n"); // use fprint and stderr
 		return 1;
 	}
     
+    // TODO: use num_expenses
     for (int i = 0; i < MAX_STACK_SIZE; i++) { //list of values are saved in the file
 	    fprintf(fp, "€%.2f\n", array[i]);
     }
@@ -60,9 +65,12 @@ int main()
     
     //values are added up
     
+    // TODO: int total = 0; // (this is the simplest way, memorize it)
+    
     for (int i = 0; i < MAX_STACK_SIZE; i++) {
         //the calculation is made with the algorithm for summing all the numbers together
-        array[i] = array[i - 1] + array[i]; 
+        array[i] = array[i - 1] + array[i];
+        // TODO: total += array[i];
     }
     
     //the total is printed
