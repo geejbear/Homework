1e97c44b92dfa449180fb54683131b0413c15968
Guille <PizzaPaganini@Guilles-MacBook-Pro.local>
2021-10-07 22:44:46 +0200

    solve reverse.c

--- reverse.c
+++ reverse.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-#define STR_LEN 12
+#define STR_LEN 20
 
 int main()
 {
-    char str1[STR_LEN] = "Reverse me!";
+    char str1[STR_LEN] = "reverse me, baby!";
+    for (int i = STR_LEN - 1; i > 0; i--) {
+        printf("%c",str1[i]);
+    }
     
     return 0;
 }
--- templates/sdlmain.c
+++ templates/sdlmain.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
+//#include <textmode.h>
 #include <stdbool.h>
 
 int main()
