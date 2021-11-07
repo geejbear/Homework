cbc141aecc2d2a6d352aaba8a86ed6566149681a
Guille <PizzaPaganini@Guilles-MacBook-Pro.local>
2021-11-07 16:56:04 +0100

    snake code in progress

--- snake/snake.c
+++ snake/snake.c
@@ -21,13 +21,17 @@
 #define CONSOLE_W 20
 #define CONSOLE_H 20
 #define CONSOLE_SIZE (CONSOLE_W * CONSOLE_H)
+
+#define MIN_X 0
+#define MIN_Y 0
+
 #define TRUE 1
 #define FALSE 0
+
 #define FPS 30
 #define SECONDS(x) (int)((float)(x) * (float)FPS)
 #define UPDATE_INTERVAL_TICKS 15 // how often (interval) does game update
 #define SNAKE_INITIAL_LENGTH 4 
-
 
 // example: probably better than a macro
 int SecondsToTicks(float seconds)
@@ -40,13 +44,25 @@ typedef struct {
     int y;
 } Point;
 
+typedef struct {
+    int x;
+    int y;
+    int ch;
+    int fc;
+    int bc;
+    bool active;
+} Entity;
+
 // snake data structure
 #define SNAKE_MAX_LEN CONSOLE_SIZE
 int dx, dy;
 Point snake[SNAKE_MAX_LEN];
 
+int apples_eaten = SNAKE_INITIAL_LENGTH;  
+
 int main()
 {
+    //init console
     if ( SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0 ) {
         fprintf(stderr, "Error: SDL_Init() failed! (%s)\n", SDL_GetError());
         return EXIT_FAILURE;
@@ -56,15 +72,36 @@ int main()
     DOS_SetScreenScale(3);
     DOS_SetCursorType(DOS_CURSOR_NONE);
     
+    //===================================================
+    
     // initial snake
-    dx = dy = 0;
+    char snake_ch = 0xB1;
+    int snake_cl = DOS_GREEN;
+
     for ( int i = 0; i < SNAKE_MAX_LEN; i++ ) {
         snake[i].x = CONSOLE_W / 2;
         snake[i].y = CONSOLE_H / 2;
     }
+    //init apple
+    srand((unsigned)time(NULL));
     
-    // program loop:
+    Entity apple = {
+        .x = rand() % CONSOLE_W,
+        .y = rand() % CONSOLE_H,
+        .ch = 0x95,
+        .fc = DOS_RED,
+        .bc = 0,
+        .active = true,
+    };
 
+    DOS_GotoXY(apple.x, apple.y);
+    DOS_SetForeground(apple.fc);
+    DOS_PrintChar(apple.ch);  
+
+    //===================================================
+
+    // program loop:
+    dx = dy = 0;
     int ticks = 0;
     bool running = true;
     while ( running ) {
@@ -106,46 +143,56 @@ int main()
             }
         }
 
-        // 2) SIMULATE/UPDATE GAME
-        
-        if ( ticks % SECONDS(0.5f) == 0 ) {
+        // clamp-like code where snake bounces off the walls.
+        if ( snake[0].x == MIN_X || snake[0].x == CONSOLE_W - 1 ) {
+            dx = -dx;
+        }
+        if ( snake[0].y == MIN_Y || snake[0].y == CONSOLE_H - 1 ) {
+            dy = -dy;
+        }
+        //===============================================
 
+        // 2) SIMULATE/UPDATE GAME
+        //printf("%d\n", apples_eaten); //debug
+
+        if ( apple.x == snake[0].x && apple.y == snake[0].y ) {
+            apple.active = false;
+            apples_eaten++;        
+            //TODO: print new apple before increasing array.
+        }
+
+        if ( ticks % SECONDS(0.3f) == 0 ) {
             // temp: dx dy affects head of snake
             snake[0].x += dx;
             snake[0].y += dy;
             
-            for ( int index = 0; index < SNAKE_MAX_LEN; index++ ) {
-                DOS_GotoXY(snake[index].x, snake[index].y);
-                DOS_SetForeground(DOS_BLUE);
-                DOS_PrintChar(219);
-            }
+            for ( int i = SNAKE_MAX_LEN; i > 0; i-- ) {
+                snake[i].x = snake[i-1].x;
+                snake[i].y = snake[i-1].y;
+            } 
         }
-              
+
+        //===============================================
+           
         // 3) RENDER GAME
         DOS_ClearScreen();
         
-        // draw player
+        // draw snake
         
-        //printf("-------------\n");
-        for ( int i = 0; i < SNAKE_INITIAL_LENGTH; i++) {
-            //printf("snake segment %d position: (%d, %d)\n", i, snake[i].x, snake[i].y);
+        for ( int i = 0; i < apples_eaten; i++ ) {
             DOS_GotoXY(snake[i].x, snake[i].y);
-            DOS_SetForeground(DOS_GREEN);
-            DOS_PrintChar(219);
+            DOS_SetForeground(snake_cl);
+            DOS_PrintChar(snake_ch);
+        }            
+            
+        // TEMP: draw apple
+
+        if ( apple.active ) {
+            DOS_GotoXY(apple.x, apple.y);
+            DOS_SetForeground(apple.fc);
+            DOS_PrintChar(apple.ch);  
         }
         
-        // TEMP: draw apple
-        int apple_x = CONSOLE_W / 4;
-        int apple_y = CONSOLE_H / 4;
-
-        char apple_ch = 'A';
-        int apple_cl = DOS_RED;
-
-        DOS_GotoXY(apple_x, apple_y);
-        DOS_SetForeground(apple_cl);
-        DOS_PrintChar(apple_ch);  
-        
-
         DOS_DrawScreen();
     }
     
