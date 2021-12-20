722eb8b586f421c8e8f5ac10321c7146e62e9118
Guille <PizzaPaganini@Guilles-MacBook-Pro.local>
2021-12-20 14:19:28 +0100

    scratch version. simple version

--- snake/snake.c
+++ snake/snake.c
@@ -1,4 +1,3 @@
-#include <play.h>
 #include <utility.h>
 
 #include <stdio.h>
@@ -6,42 +5,72 @@
 #include <SDL2/SDL.h>
 #include <textmode.h>
 #include <stdbool.h>
-#include <time.h>
 
-// TODO: snake movement operation/algorithm
-// TODO: plan apple eating logic
-// Plan: (take stock of everything)
-// when snake head x, y == apple x, y:
-// - snake len ++
-// - new apple i.e. change apple location (random)
-// TODO: snake death
-// Plan:
-// -
 
-#define CONSOLE_W 15
 #define CONSOLE_H 15
-#define CONSOLE_SIZE (CONSOLE_W * CONSOLE_H)
-
+#define CONSOLE_W 15
 #define MIN_X 0
 #define MIN_Y 0
-
 #define FPS 30
-#define SECONDS(x) (int)((float)(x) * (float)FPS)
-#define UPDATE_INTERVAL_TICKS 15 // how often (interval) does game update
-#define SNAKE_INITIAL_LENGTH 3 
-#define SNAKE_MAX_LEN CONSOLE_SIZE
+#define SNAKE_ARRAY_SIZE 100
 
-void RenderApple(int x, int y, char ch, int fc) // Apple is an entity
+typedef struct 
 {
-    DOS_GotoXY(x, y);
-    DOS_SetForeground(fc);
-    DOS_PrintChar(ch); 
+    int x;
+    int y;
+    int dx;
+    int dy;
+    char ch;
+    int fc;
+    bool active;
+}Entity;
+
+Entity NewEntity(Entity *e, int x, int y, int dx, int dy, char ch, int color) 
+{
+    e->x = x;
+    e->y = y;
+    e->dx = dx;
+    e->dy = dy;
+    e->ch = ch;
+    e->fc = color;
+    e->active = true;
+
+    return *e;
 }
 
-// example: probably better than a macro
-int SecondsToTicks(float seconds)
+void RenderEntity(Entity *e) 
 {
-    return seconds * (float)FPS;
+    if ( e->active ) {
+        DOS_GotoXY(e->x, e->y);
+        DOS_SetForeground(e->fc);
+        DOS_PrintChar(e->ch);
+    }
+}
+
+void RenderArrayEntity(Entity *e, int value) 
+{
+    if ( e->active ) {
+        for ( int i = 0; i < value; i++ ) {
+            DOS_GotoXY(e->x, e->y);
+            DOS_SetForeground(e->fc);
+            DOS_PrintChar(e->ch);
+        }
+    }
+}
+
+int IsSDLInit() 
+{
+    if ( SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0 ) {
+        fprintf(stderr, "Error: SDL_Init() failed! (%s)\n", SDL_GetError());
+    }
+    return EXIT_FAILURE;
+}
+
+void InitScreen()
+{
+    DOS_InitScreen("Bounder", CONSOLE_W, CONSOLE_H, DOS_MODE40, 4);
+    DOS_SetScreenScale(3);
+    DOS_SetCursorType(DOS_CURSOR_NONE);
 }
 
 void CollectAppleSoundEffect(void)
@@ -51,271 +80,107 @@ void CollectAppleSoundEffect(void)
     DOS_Sound(1720, 70);
 }
 
-void RenderSnakeBackToStart(int point_x[], int point_y[])
-{
-    for ( int i = 0; i < SNAKE_MAX_LEN; i++ ) {
-        point_x[i] = CONSOLE_W / 2;
-        point_y[i] = CONSOLE_H / 2 + i;
+void WarpToOppositeEnd(int *x, int *y, int max_x, int max_y, int min_x, int min_y)
+{   
+    if ( *x == max_x ) {
+        *x = min_x + 1;
     }
+    if ( *y == max_x ) {
+        *y = min_y + 1;
+    }
+    if ( *x == min_x ) {
+        *x = max_x - 1;
+    }
+    if ( *y == min_y ) {
+        *y = max_y - 1;
+    }
+ 
 }
 
-typedef struct {
-    int x;
-    int y;
-    bool active; // TF: no
-} Point;
-
-typedef struct {
-    int x;
-    int y;
-    int ch;
-    int fc;
-    int bc;
-    bool active;
-} Entity;
-
-// snake data structure
-int dx, dy;
-Point snake[SNAKE_MAX_LEN];
-int snake_len = SNAKE_INITIAL_LENGTH; // num of segments in snake
-
-#if 0
-int map[CONSOLE_H][CONSOLE_W] = {
-    { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
-    { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-    { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-    { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-    { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-    { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-    { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-    { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-    { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-    { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-    { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-    { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-    { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-    { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-    { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
-};
-#endif
-
-/* void Input(int key, bool running, SDL_Event event)
-{
-    switch ( key ) {
-
-        case SDLK_ESCAPE:
-            if ( event.key.keysym.sym == SDLK_ESCAPE ) {
-                running = false;
-            }
-            break;
-        case SDLK_UP:
-            if ( event.key.keysym.sym == SDLK_UP ) {
-                dx = 0;
-                dy = -1;
-            }
-            break;
-        case SDLK_LEFT:
-            if ( event.key.keysym.sym == SDLK_LEFT ) {
-                dx = -1;
-                dy = 0;
-            }
-            break;
-        case SDLK_RIGHT:
-            if ( event.key.keysym.sym == SDLK_RIGHT ) {
-                dx = +1;
-                dy = 0;
-            }
-            break;
-        case SDLK_DOWN:
-            if ( event.key.keysym.sym == SDLK_DOWN ) {
-                dx = 0;
-                dy = +1;
-            }
-            break;
-        default:
-            break;
-    }
-} */
-
-void DrawSnake() {
-    for ( int i = 0; i < snake_len; i++ ) {
-        DOS_GotoXY(snake[i].x, snake[i].y);
-        DOS_SetForeground(DOS_GREEN);
-        DOS_PrintChar(0xB1);
-    }
-}
 
 int main()
 {
     //init console
-    if ( SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0 ) {
-        fprintf(stderr, "Error: SDL_Init() failed! (%s)\n", SDL_GetError());
-        return EXIT_FAILURE;
-    }
+    IsSDLInit();
+    InitScreen();
     
-    DOS_InitScreen("Bounder", CONSOLE_W, CONSOLE_H, DOS_MODE40, 4);
-    //DOS_InitSound();
-    DOS_SetScreenScale(3);
-    DOS_SetCursorType(DOS_CURSOR_NONE);
-    
-    //===================================================
-    
-    // initial snake
-    snake[snake_len].active = true; // ??
-    
-    for ( int i = 0; i < snake_len; i++ ) {
-        snake[i].x = CONSOLE_W / 2;
-        snake[i].y = CONSOLE_H / 2 + i; 
-    }
-    //init apple
     srand((unsigned)time(NULL));
+    int snake_len = 4;
     
-    Entity apple = {
-        .x = rand() % CONSOLE_W,
-        .y = rand() % CONSOLE_H,
-        .ch = 0x95,
-        .fc = DOS_RED,
-        .bc = 0,
-        .active = true,
-    };
-
+    Entity snake[SNAKE_ARRAY_SIZE];
+    NewEntity(&snake[snake_len], CONSOLE_W / 2, CONSOLE_H / 2, 0, 0, 0xB1, DOS_GREEN);
     
-    //RenderApple(apple.x, apple.y, apple.ch, apple.fc);
-
-    //===================================================
-
-    // program loop:
-    dx = 0, dy = -1; // TF: game design: dx/dy both 0
-    int ticks = 0;
+    
+    Entity apple;
+    NewEntity(&apple, rand() % CONSOLE_H, rand() % CONSOLE_W, 0, 0, 0x95, DOS_RED);
+    
+    
     bool running = true;
+    int ticks = 0;
     while ( running ) {
         ticks++;
-        
+
         DOS_LimitFrameRate(FPS);
-        
-        // 1) GET INPUT
+         // 1) GET INPUT
         SDL_Event event;
         while ( SDL_PollEvent(&event) ) {
             switch ( event.type ) {
                 case SDL_QUIT:
                     running = false;
-                    break;
+                    break;  
                 case SDL_KEYDOWN:
                     //Input(SDL_KEYDOWN, running, event);
                     if ( event.key.keysym.sym == SDLK_ESCAPE ) {
                         running = false;
                     }
                     if ( event.key.keysym.sym == SDLK_UP ) {
-                        dx = 0;
-                        dy = -1;
+                        snake[snake_len].dx = 0;
+                        snake[snake_len].dy = -1;
                     }
                     else if ( event.key.keysym.sym == SDLK_LEFT ) {
-                        dx = -1;
-                        dy = 0;
+                        snake[snake_len].dx = -1;
+                        snake[snake_len].dy = 0;
                     }
                     else if ( event.key.keysym.sym == SDLK_RIGHT ) {
-                        dx = +1;
-                        dy = 0;
+                        snake[snake_len].dx = +1;
+                        snake[snake_len].dy = 0;
                     }
                     else if ( event.key.keysym.sym == SDLK_DOWN ) {
-                        dx = 0;
-                        dy = +1;
+                        snake[snake_len].dx = 0;
+                        snake[snake_len].dy = +1;
                     }
                     break;
                 default:
                     break;
-                }
-        }
-
-        // make snake "wrap around" to other side of screen
-#if 0
-        if (( snake[0].x == MIN_X  || snake[0].x == CONSOLE_W - 1 ) || 
-             ( snake[0].y == MIN_Y || snake[0].y == CONSOLE_H - 1)) {
-            snake_len = SNAKE_INITIAL_LENGTH;
-            
-        }
-
-        //if head collides with body, snakes jumps back to the start position
-        for ( int i = snake_len; i > 1; i-- ) {
-            if (( snake[0].x == snake[i].x) && (snake[0].y == snake[i].y )) {
-                
-                snake_len = SNAKE_INITIAL_LENGTH;
-                
             }
+        }    
+       //Update game
+
+        if ( ticks % 10 == 0 ) {
+            snake[snake_len].x += snake[snake_len].dx;
+            snake[snake_len].y += snake[snake_len].dy;
         }
-#endif
         
-        //===============================================
-
-        // 2) SIMULATE/UPDATE GAME
-
-        if ( apple.x == snake[0].x && apple.y == snake[0].y ) {
-            // eat the apple
+        if ( apple.x == snake[snake_len].x && apple.y == snake[snake_len].y ) {
             CollectAppleSoundEffect();
-            
             apple.x = rand() % CONSOLE_W;
-            apple.y = rand() % CONSOLE_H;
-            
-            snake_len++;
+            apple.y = rand() % CONSOLE_H; 
 
-        }
+        }  
 
-        // move the snake
-        if ( ticks % SECONDS(0.3f) == 0 ) {
-            // temp: dx dy affects head of snake
-            for ( int i = SNAKE_MAX_LEN; i > 0; i-- ) {
-                snake[i].x = snake[i-1].x;
-                snake[i].y = snake[i-1].y;
-            } 
-            snake[0].x += dx;
-            snake[0].y += dy;
-        }
-
-        //===============================================
-        // 3) RENDER GAME
-        
+        WarpToOppositeEnd(&snake[snake_len].x, &snake[snake_len].y, CONSOLE_W, CONSOLE_H, MIN_X, MIN_Y);
+      
+        //Draw game
         DOS_ClearScreen();
-        RenderApple(apple.x, apple.y, apple.ch, apple.fc);
-        DrawSnake();
+       
+        RenderArrayEntity(&snake[snake_len], snake_len);
+        
+        RenderEntity(&apple);
+
         DOS_DrawScreen();
-    }
-    
-    // exit program:
+    }    
     
     SDL_Quit();
     
     return EXIT_SUCCESS;
 }
-
-
-
-
-
-// was in drawing section, should be in update section
-#if 0
-        if ( apple.active )  {
-            for ( int i = snake_len; i > 1; i-- ) {
-                if (( apple.x != snake[i].x ) && ( apple.y != snake[i].y )) {
-                        RenderApple(apple.x, apple.y, apple.ch, apple.fc);
-                }
-            }
-        }
-#endif
-
-
-
-
-
-// drawing map array
-#if 0
-        int y, x = 0; // TF: make local
-        for ( y = 0; y < CONSOLE_H; y++ ) {
-            for ( x = 0; x < CONSOLE_W; x++ ) {
-                if (map[y][x]) {
-                    DOS_GotoXY(x, y);
-                    DOS_SetForeground(DOS_GRAY);
-                    DOS_PrintChar(219);
-                }
-            }
-        }
-#endif
